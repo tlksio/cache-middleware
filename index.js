70579ca35b65de53a08cbbed5b3ad9e3a1e84547
Raül Pérez <raul.perez@r3labs.io>
2015-06-14 02:51:22 +0200

    Default options

--- index.js
+++ index.js
@@ -8,21 +8,24 @@ function serveFileStream() {
     fs.createReadStream(filePath).pipe(res);
 }
 
-exports.cache = function() {
+module.exports = function cache(options) {
     'use strict';
+    options = options || {
+        "path": "/tmp",
+        "ttl": 3600000
+    };
     return function(req, res, next) {
         // Check if the user is logged
         if (req.session.user === undefined) {
             // serve cache only to registered users
-            var miliseconds = 3600000; // 1h
             var fileName = md5(req.url);
-            var filePath = __dirname + "/cache/" + fileName;
+            var filePath = options.path + fileName;
             fs.exists(filePath, function(exists) {
                 if (exists) {
                     fs.stat(filePath, function(err, stats) {
                         var now = new Date();
                         var diff = now - stats.mtime;
-                        if (diff < miliseconds) {
+                        if (diff < options.ttl) {
                             serveFileStream();
                         } else {
                             console.log("Old cache file", req.url, fileName);
