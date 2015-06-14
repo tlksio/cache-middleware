85a2ab06bb471dc1cd99e2cbbc567cc7b2a0888d
Raül Pérez <raul.perez@r3labs.io>
2015-06-14 02:39:34 +0200

    middleware returns a function

--- index.js
+++ index.js
@@ -7,34 +7,36 @@ function serveFileStream() {
     fs.createReadStream(filePath).pipe(res);
 }
 
-exports.cache = function(req, res, next) {
+exports.cache = function() {
     'use strict';
-    // Check if the user is logged
-    if (req.session.user === undefined) {
-        // serve cache only to registered users
-        var miliseconds = 3600000; // 1h
-        var fileName = md5(req.url);
-        var filePath = __dirname + "/cache/" + fileName;
-        fs.exists(filePath, function(exists) {
-            if (exists) {
-                fs.stat(filePath, function(err, stats) {
-                    var now = new Date();
-                    var diff = now - stats.mtime;
-                    if (diff < miliseconds) {
-                        serveFileStream();
-                    } else {
-                        console.log("Old cache file", req.url, fileName);
-                        next();
-                    }
-                });
-            } else {
-                console.log("Not cached", req.url);
-                next();
-            }
-        });
-    } else {
-        console.log("User session - not cached", req.url);
-        next();
-    }
+    return function(req, res, next) {
+        // Check if the user is logged
+        if (req.session.user === undefined) {
+            // serve cache only to registered users
+            var miliseconds = 3600000; // 1h
+            var fileName = md5(req.url);
+            var filePath = __dirname + "/cache/" + fileName;
+            fs.exists(filePath, function(exists) {
+                if (exists) {
+                    fs.stat(filePath, function(err, stats) {
+                        var now = new Date();
+                        var diff = now - stats.mtime;
+                        if (diff < miliseconds) {
+                            serveFileStream();
+                        } else {
+                            console.log("Old cache file", req.url, fileName);
+                            next();
+                        }
+                    });
+                } else {
+                    console.log("Not cached", req.url);
+                    next();
+                }
+            });
+        } else {
+            console.log("User session - not cached", req.url);
+            next();
+        }
 
-};
+    }
+}
